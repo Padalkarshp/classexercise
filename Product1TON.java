a37aab1aea6bafaf7db03a47c717de5491acd5ac
Padalkarshp <120395182+Padalkarshp@users.noreply.github.com>
2022-12-15 12:23:05 +0530

    Add files via upload

---
+++ Product1TON.java
@@ -0,0 +1,26 @@
+
+public class Product1TON {
+
+	public static void main(String[] args) {
+		long product=1;
+		
+		for(int i=1;i<=10;i++)
+		{
+			product=product*i;
+		}
+		System.out.println("Product form 1 to 10 is " +product);
+		
+		for(int i=11;i<=14;i++)
+		{
+			product=product*i;
+			System.out.println("Product form 1 to " +i+ " is " +product);
+		}
+		
+
+}
+
+
+
+	}
+
+
