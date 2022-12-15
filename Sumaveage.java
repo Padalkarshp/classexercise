ab58b47b22fc9d2c9e3ecb37ed10c7e53ab40b8b
Padalkarshp <120395182+Padalkarshp@users.noreply.github.com>
2022-12-15 12:20:07 +0530

    Add files via upload

---
+++ Sumaveage.java
@@ -0,0 +1,21 @@
+
+public class Sumaveage {
+
+	public static void main(String[] args) {
+		int sum=0;
+		
+		for(int i=1;i<=100;i++)
+		{
+			sum=sum+i;
+		}
+		
+		double avg=(float)sum/100;
+		
+		System.out.println("The Sum is  "+sum);
+		System.out.println("The Average is  "+avg);
+
+
+
+	}
+
+}
