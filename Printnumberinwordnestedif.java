c7169dbfa2da5a90aa49f72d74e49309aab747c7
Padalkarshp <120395182+Padalkarshp@users.noreply.github.com>
2022-12-15 11:56:00 +0530

    Add files via upload

---
+++ Printnumberinwordnestedif.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Printnumberinwordnestedif {
+
+	public static void main(String[] args) {
+		  int num;
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the Number:");
+	        num = sc.nextInt();
+
+	        {
+	            if (num == 1)
+	                System.out.println("ONE");
+	            else if (num == 2)
+	                System.out.println("TWO");
+	            else if (num == 3)
+	                System.out.println("THREE");
+	            else if (num == 4)
+	                System.out.println("FOUR");
+	            else if (num == 5)
+	                System.out.println("FIVE");
+	            else if (num == 6)
+	                System.out.println("SIX");
+	            else if (num == 7)
+	                System.out.println("SEVEN");
+	            else if (num == 8)
+	                System.out.println("EIGHT");
+	            else if (num == 9)
+	                System.out.println("NINE");
+	            else
+	                System.out.println("OTHER");
+	        }
+
+	}
+
+}
